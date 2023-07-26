56370e7973de06c9d3f9f9b91dde2ed73bb35860
MichaelSault <michael_sault@hotmail.com>
2023-07-25 23:58:56 -0400

    started working on edit functionality for the guest list

--- src/components/GuestList.jsx
+++ src/components/GuestList.jsx
@@ -10,6 +10,25 @@ function GuestList() {
   const [guests, setGuests] = useState([]);
   const [updatedGuest, setUpdatedGuest] = useState([]);
 
+  useEffect(() => {
+    axios.get("http://localhost:3001/posts")
+    .then(res => {
+        console.log(res);
+        setGuests(res.data);
+    })
+    .catch((err) => console.log(err));
+    }, [])
+
+    const saveUpdatedPost = () => {
+        console.log(updatedGuest);
+        axios.put(`http://localhost:3001/update/${updatedGuest._id}`, updatedGuest)
+        .then(res => console.log(res))
+        .catch((err) => console.Console.log(err));
+
+        //handleClose();
+        //window.location.reload();
+    };
+
 
   return (
     <>
