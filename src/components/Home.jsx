f31003b1d7f328832b4952f040f8a18f67312918
MichaelSault <michael_sault@hotmail.com>
2023-09-03 23:57:17 -0400

    added a carousel element for engagement photos

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -10,34 +10,62 @@ function Home() {
   return (
     
     <>
-        {/* <div id="carouselExampleIndicators" class="carousel slide" data-bs-ride="true">
-            <div class="carousel-indicators">
-            <button type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide-to="0" class="active" aria-current="true" aria-label="Slide 1"></button>
-            <button type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide-to="1" aria-label="Slide 2"></button>
-            <button type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide-to="2" aria-label="Slide 3"></button>
+        <div id="carouselExampleFade" className="carousel slide carousel-fade" data-bs-ride="carousel">
+            <div className="carousel-indicators">
+                <button type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide-to="0" className="active" aria-current="true" aria-label="Slide 1"></button>
+                <button type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide-to="1" aria-label="Slide 2"></button>
+                <button type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide-to="2" aria-label="Slide 3"></button>
+                <button type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide-to="3" aria-label="Slide 4"></button>
+                <button type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide-to="4" aria-label="Slide 5"></button>
+                <button type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide-to="5" aria-label="Slide 6"></button>
+                <button type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide-to="6" aria-label="Slide 7"></button>
+                <button type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide-to="7" aria-label="Slide 8"></button>
+                <button type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide-to="8" aria-label="Slide 9"></button>
+                <button type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide-to="9" aria-label="Slide 10"></button>
             </div>
-            <div class="carousel-inner">
-            <div class="carousel-item active">
-                <img src="..." class="d-block w-100" alt="..." />
+            <div className="carousel-inner">
+                <div className="carousel-item active">
+                    <img src="src/assets/CarouselPhotos/TanyaWedding1.jpg" className="d-block" width="500" height="500" alt="..."/>
+                </div>
+                <div className="carousel-item">
+                    <img src="src/assets/CarouselPhotos/TanyaWedding2.jpg" className="d-block" width="500" height="500" alt="..."/>
+                </div>
+                <div className="carousel-item">
+                    <img src="src/assets/CarouselPhotos/TanyaWedding3.jpg" className="d-block" width="500" height="500" alt="..."/>
+                </div>
+                <div className="carousel-item">
+                    <img src="src/assets/CarouselPhotos/TanyaWedding4.jpg" className="d-block" width="500" height="500" alt="..."/>
+                </div>
+                <div className="carousel-item">
+                    <img src="src/assets/CarouselPhotos/TanyaWedding5.jpg" className="d-block" width="500" height="500" alt="..."/>
+                </div>
+                <div className="carousel-item">
+                    <img src="src/assets/CarouselPhotos/TanyaWedding6.jpg" className="d-block" width="500" height="500" alt="..."/>
+                </div>
+                <div className="carousel-item">
+                    <img src="src/assets/CarouselPhotos/TanyaWedding7.jpg" className="d-block" width="500" height="500" alt="..."/>
+                </div>
+                <div className="carousel-item">
+                    <img src="src/assets/CarouselPhotos/TanyaWedding8.jpg" className="d-block" width="500" height="500" alt="..."/>
+                </div>
+                <div className="carousel-item">
+                    <img src="src/assets/CarouselPhotos/TanyaWedding9.jpg" className="d-block" width="500" height="500" alt="..."/>
+                </div>
+                <div className="carousel-item">
+                    <img src="src/assets/CarouselPhotos/TanyaWedding10.jpg" className="d-block" width="500" height="500" alt="..."/>
+                </div>
             </div>
-            <div class="carousel-item">
-                <img src="..." class="d-block w-100" alt="..." />
-            </div>
-            <div class="carousel-item">
-                <img src="..." class="d-block w-100" alt="..." />
-            </div>
-            </div>
-            <button class="carousel-control-prev" type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide="prev">
-            <span class="carousel-control-prev-icon" aria-hidden="true"></span>
-            <span class="visually-hidden">Previous</span>
+            <button className="carousel-control-prev" type="button" data-bs-target="#carouselExampleControls" data-bs-slide="prev">
+                <span className="carousel-control-prev-icon" aria-hidden="true"></span>
+                <span className="visually-hidden">Previous</span>
             </button>
-            <button class="carousel-control-next" type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide="next">
-            <span class="carousel-control-next-icon" aria-hidden="true"></span>
-            <span class="visually-hidden">Next</span>
+            <button className="carousel-control-next" type="button" data-bs-target="#carouselExampleControls" data-bs-slide="next">
+                <span className="carousel-control-next-icon" aria-hidden="true"></span>
+                <span className="visually-hidden">Next</span>
             </button>
-        </div> */}
+        </div>
 
-        <Accordion defaultActiveKey="0" style={{width:"1000px"}}>
+        <Accordion defaultActiveKey="0" style={{width:"500px"}}>
             <Accordion.Item eventKey="1">
                 <Accordion.Header>Reception Party</Accordion.Header>
                 <Accordion.Body>
