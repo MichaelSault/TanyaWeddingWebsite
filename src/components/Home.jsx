3f6443eec3e40d43358347d5006948c544ca6367
MichaelSault <michael_sault@hotmail.com>
2023-09-04 23:59:25 -0400

    fixed visual glitch with the carousel moving on image switch

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -12,54 +12,54 @@ function Home() {
     <>
         <div id="carouselExampleFade" className="carousel slide carousel-fade" data-bs-ride="carousel">
             <div className="carousel-indicators">
-                <button type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide-to="0" className="active" aria-current="true" aria-label="Slide 1"></button>
-                <button type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide-to="1" aria-label="Slide 2"></button>
-                <button type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide-to="2" aria-label="Slide 3"></button>
-                <button type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide-to="3" aria-label="Slide 4"></button>
-                <button type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide-to="4" aria-label="Slide 5"></button>
-                <button type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide-to="5" aria-label="Slide 6"></button>
-                <button type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide-to="6" aria-label="Slide 7"></button>
-                <button type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide-to="7" aria-label="Slide 8"></button>
-                <button type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide-to="8" aria-label="Slide 9"></button>
-                <button type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide-to="9" aria-label="Slide 10"></button>
+                <button type="button" data-bs-target="#carouselExampleFade" data-bs-slide-to="0" className="active" aria-current="true" aria-label="Slide 1"></button>
+                <button type="button" data-bs-target="#carouselExampleFade" data-bs-slide-to="1" aria-label="Slide 2"></button>
+                <button type="button" data-bs-target="#carouselExampleFade" data-bs-slide-to="2" aria-label="Slide 3"></button>
+                <button type="button" data-bs-target="#carouselExampleFade" data-bs-slide-to="3" aria-label="Slide 4"></button>
+                <button type="button" data-bs-target="#carouselExampleFade" data-bs-slide-to="4" aria-label="Slide 5"></button>
+                <button type="button" data-bs-target="#carouselExampleFade" data-bs-slide-to="5" aria-label="Slide 6"></button>
+                <button type="button" data-bs-target="#carouselExampleFade" data-bs-slide-to="6" aria-label="Slide 7"></button>
+                <button type="button" data-bs-target="#carouselExampleFade" data-bs-slide-to="7" aria-label="Slide 8"></button>
+                <button type="button" data-bs-target="#carouselExampleFade" data-bs-slide-to="8" aria-label="Slide 9"></button>
+                <button type="button" data-bs-target="#carouselExampleFade" data-bs-slide-to="9" aria-label="Slide 10"></button>
             </div>
             <div className="carousel-inner">
                 <div className="carousel-item active">
-                    <img src="src/assets/CarouselPhotos/TanyaWedding1.jpg" className="d-block" width="500" height="500" alt="..."/>
+                    <img src="src/assets/CarouselPhotos/TanyaWedding1.jpg" className="d-block" width="100%" height="500px" alt="..."/>
                 </div>
                 <div className="carousel-item">
-                    <img src="src/assets/CarouselPhotos/TanyaWedding2.jpg" className="d-block" width="500" height="500" alt="..."/>
+                    <img src="src/assets/CarouselPhotos/TanyaWedding2.jpg" className="d-block" width="500px" height="500px" alt="..."/>
                 </div>
                 <div className="carousel-item">
-                    <img src="src/assets/CarouselPhotos/TanyaWedding3.jpg" className="d-block" width="500" height="500" alt="..."/>
+                    <img src="src/assets/CarouselPhotos/TanyaWedding3.jpg" className="d-block" width="500px" height="500px" alt="..."/>
                 </div>
                 <div className="carousel-item">
-                    <img src="src/assets/CarouselPhotos/TanyaWedding4.jpg" className="d-block" width="500" height="500" alt="..."/>
+                    <img src="src/assets/CarouselPhotos/TanyaWedding4.jpg" className="d-block" width="500px" height="500px" alt="..."/>
                 </div>
                 <div className="carousel-item">
-                    <img src="src/assets/CarouselPhotos/TanyaWedding5.jpg" className="d-block" width="500" height="500" alt="..."/>
+                    <img src="src/assets/CarouselPhotos/TanyaWedding5.jpg" className="d-block" width="500px" height="500px" alt="..."/>
                 </div>
                 <div className="carousel-item">
-                    <img src="src/assets/CarouselPhotos/TanyaWedding6.jpg" className="d-block" width="500" height="500" alt="..."/>
+                    <img src="src/assets/CarouselPhotos/TanyaWedding6.jpg" className="d-block" width="500px" height="500px" alt="..."/>
                 </div>
                 <div className="carousel-item">
-                    <img src="src/assets/CarouselPhotos/TanyaWedding7.jpg" className="d-block" width="500" height="500" alt="..."/>
+                    <img src="src/assets/CarouselPhotos/TanyaWedding7.jpg" className="d-block" width="500px" height="500px" alt="..."/>
                 </div>
                 <div className="carousel-item">
-                    <img src="src/assets/CarouselPhotos/TanyaWedding8.jpg" className="d-block" width="500" height="500" alt="..."/>
+                    <img src="src/assets/CarouselPhotos/TanyaWedding8.jpg" className="d-block" width="500px" height="500px" alt="..."/>
                 </div>
                 <div className="carousel-item">
-                    <img src="src/assets/CarouselPhotos/TanyaWedding9.jpg" className="d-block" width="500" height="500" alt="..."/>
+                    <img src="src/assets/CarouselPhotos/TanyaWedding9.jpg" className="d-block" width="500px" height="500px" alt="..."/>
                 </div>
                 <div className="carousel-item">
-                    <img src="src/assets/CarouselPhotos/TanyaWedding10.jpg" className="d-block" width="500" height="500" alt="..."/>
+                    <img src="src/assets/CarouselPhotos/TanyaWedding10.jpg" className="d-block" width="500px" height="500px" alt="..."/>
                 </div>
             </div>
-            <button className="carousel-control-prev" type="button" data-bs-target="#carouselExampleControls" data-bs-slide="prev">
+            <button className="carousel-control-prev" type="button" data-bs-target="#carouselExampleFade" data-bs-slide="prev">
                 <span className="carousel-control-prev-icon" aria-hidden="true"></span>
                 <span className="visually-hidden">Previous</span>
             </button>
-            <button className="carousel-control-next" type="button" data-bs-target="#carouselExampleControls" data-bs-slide="next">
+            <button className="carousel-control-next" type="button" data-bs-target="#carouselExampleFade" data-bs-slide="next">
                 <span className="carousel-control-next-icon" aria-hidden="true"></span>
                 <span className="visually-hidden">Next</span>
             </button>
