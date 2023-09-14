cb38e02e188001cc4d0456a71393294b3de7ff52
MichaelSault <michael_sault@hotmail.com>
2023-09-13 23:52:20 -0400

    added variables to event banner class

--- src/components/EventBanner.jsx
+++ src/components/EventBanner.jsx
@@ -1,23 +1,35 @@
-import '../App.css'
+import { forwardRef } from 'react';
+import '../App.css';
 
-function EventBanner() {
+// eslint-disable-next-line react/display-name
+const EventBanner = forwardRef(
+  (
+    {
+      event,
+      date,
+      location,
+      details
+    },
+    ref
+
+  ) => {
 
   return (
-    
     <>
       <div className='EventHeader' width='100%'>
         <div className='eventName'>
-            <h1 id='eventTitle'>Wedding Reception</h1>
+            <h1 id='eventTitle'>{event}</h1>
         </div>
         <div className='eventDetails'>
-            <h1 id='eventText'>Date: Aug 1st</h1>
-            <h1 id='eventText'>Location: Burlington</h1>
-            <h1 id='eventText'>Details: Details go here eventually</h1>
+            <div className='eventRowRight'><h1 id='eventText'>Date: </h1><h1 id='eventTextinfo'>{date}</h1></div>
+            <div className='eventRowRight'><h1 id='eventText'>Location: </h1><h1 id='eventTextinfo'>{location}</h1></div>
+            <div className='eventRowRight'><h1 id='eventText'>Details: </h1><h1 id='eventTextinfo'>{details}</h1></div>
         </div>
       </div>
 
     </>
   )
-}
+  }
+);
 
-export default EventBanner
+export default EventBanner;
--- src/components/EventBannerMirror.jsx
+++ src/components/EventBannerMirror.jsx
@@ -1,22 +1,36 @@
-import '../App.css'
+import { forwardRef } from 'react';
+import '../App.css';
 
-function EventBannerMirror() {
+// eslint-disable-next-line react/display-name
+const EventBannerMirror = forwardRef(
+  (
+    {
+      event,
+      date,
+      location,
+      details
+    },
+    ref
+
+  ) => {
 
   return (
-    
     <>
       <div className='EventHeader' width='100%'>
-        <div className='eventDetails'>
-            <h1 id='eventText'>Date: Aug 2nd</h1>
-            <h1 id='eventText'>Location: Oakville</h1>
-            <h1 id='eventText'>Details: Details go here eventually</h1>
+      <div className='eventDetails'>
+            <div className='eventRowLeft'><h1 id='eventText'>Date: </h1><h1 id='eventTextinfo'>{date}</h1></div>
+            <div className='eventRowLeft'><h1 id='eventText'>Location: </h1><h1 id='eventTextinfo'>{location}</h1></div>
+            <div className='eventRowLeft'><h1 id='eventText'>Details: </h1><h1 id='eventTextinfo'>{details}</h1></div>
         </div>
         <div className='eventName'>
-            <h1 id='eventTitle'>Wedding</h1>
+            <h1 id='eventTitle'>{event}</h1>
         </div>
+        
       </div>
+
     </>
   )
-}
+  }
+);
 
-export default EventBannerMirror
+export default EventBannerMirror;
