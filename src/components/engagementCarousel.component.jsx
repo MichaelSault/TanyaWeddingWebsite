68c380b762c8c7d328dc9930e311518e9af02293
MichaelSault <michael_sault@hotmail.com>
2024-01-20 15:54:07 -0500

    updated carousel to include all portrait photos

--- src/components/engagementCarousel.component.jsx
+++ src/components/engagementCarousel.component.jsx
@@ -4,13 +4,36 @@ import '../App.css'
 import carouselPhoto1 from '../assets/CarouselPhotos/TanyaWedding1.jpg';
 import carouselPhoto2 from '../assets/CarouselPhotos/TanyaWedding2.jpg';
 import carouselPhoto3 from '../assets/CarouselPhotos/TanyaWedding3.jpg';
-import carouselPhoto4 from '../assets/CarouselPhotos/TanyaWedding4.jpg';
-import carouselPhoto5 from '../assets/CarouselPhotos/TanyaWedding5.jpg';
-import carouselPhoto6 from '../assets/CarouselPhotos/TanyaWedding6.jpg';
-import carouselPhoto7 from '../assets/CarouselPhotos/TanyaWedding7.jpg';
-import carouselPhoto8 from '../assets/CarouselPhotos/TanyaWedding8.jpg';
-import carouselPhoto9 from '../assets/CarouselPhotos/TanyaWedding9.jpg';
-import carouselPhoto10 from '../assets/CarouselPhotos/TanyaWedding10.jpg';
+import carouselPhoto4 from '../assets/CarouselPhotos/TanyaWedding6.jpg';
+import carouselPhoto5 from '../assets/CarouselPhotos/TanyaWedding7.jpg';
+import carouselPhoto6 from '../assets/CarouselPhotos/TanyaWedding9.jpg';
+import carouselPhoto7 from '../assets/CarouselPhotos/TanyaWedding11.jpg';
+import carouselPhoto8 from '../assets/CarouselPhotos/TanyaWedding12.jpg';
+import carouselPhoto9 from '../assets/CarouselPhotos/TanyaWedding13.jpg';
+import carouselPhoto10 from '../assets/CarouselPhotos/TanyaWedding15.jpg';
+import carouselPhoto11 from '../assets/CarouselPhotos/TanyaWedding16.jpg';
+import carouselPhoto12 from '../assets/CarouselPhotos/TanyaWedding17.jpg';
+import carouselPhoto13 from '../assets/CarouselPhotos/TanyaWedding19.jpg';
+import carouselPhoto14 from '../assets/CarouselPhotos/TanyaWedding20.jpg';
+import carouselPhoto15 from '../assets/CarouselPhotos/TanyaWedding21.jpg';
+import carouselPhoto16 from '../assets/CarouselPhotos/TanyaWedding22.jpg';
+import carouselPhoto17 from '../assets/CarouselPhotos/TanyaWedding27.jpg';
+import carouselPhoto18 from '../assets/CarouselPhotos/TanyaWedding29.jpg';
+import carouselPhoto19 from '../assets/CarouselPhotos/TanyaWedding31.jpg';
+import carouselPhoto20 from '../assets/CarouselPhotos/TanyaWedding32.jpg';
+import carouselPhoto21 from '../assets/CarouselPhotos/TanyaWedding33.jpg';
+import carouselPhoto22 from '../assets/CarouselPhotos/TanyaWedding34.jpg';
+import carouselPhoto23 from '../assets/CarouselPhotos/TanyaWedding35.jpg';
+import carouselPhoto24 from '../assets/CarouselPhotos/TanyaWedding37.jpg';
+import carouselPhoto25 from '../assets/CarouselPhotos/TanyaWedding38.jpg';
+import carouselPhoto26 from '../assets/CarouselPhotos/TanyaWedding44.jpg';
+import carouselPhoto27 from '../assets/CarouselPhotos/TanyaWedding46.jpg';
+import carouselPhoto28 from '../assets/CarouselPhotos/TanyaWedding47.jpg';
+import carouselPhoto29 from '../assets/CarouselPhotos/TanyaWedding48.jpg';
+import carouselPhoto30 from '../assets/CarouselPhotos/TanyaWedding51.jpg';
+import carouselPhoto31 from '../assets/CarouselPhotos/TanyaWedding52.jpg';
+import carouselPhoto32 from '../assets/CarouselPhotos/TanyaWedding53.jpg';
+import carouselPhoto33 from '../assets/CarouselPhotos/TanyaWedding56.jpg';
 
 function EngagementCarousel() {
     return (
@@ -28,6 +51,30 @@ function EngagementCarousel() {
                   <button type="button" data-bs-target="#carouselExampleFade" data-bs-slide-to="7" aria-label="Slide 8"></button>
                   <button type="button" data-bs-target="#carouselExampleFade" data-bs-slide-to="8" aria-label="Slide 9"></button>
                   <button type="button" data-bs-target="#carouselExampleFade" data-bs-slide-to="9" aria-label="Slide 10"></button>
+                  <button type="button" data-bs-target="#carouselExampleFade" data-bs-slide-to="10" aria-label="Slide 11"></button>
+                  <button type="button" data-bs-target="#carouselExampleFade" data-bs-slide-to="11" aria-label="Slide 12"></button>
+                  <button type="button" data-bs-target="#carouselExampleFade" data-bs-slide-to="12" aria-label="Slide 13"></button>
+                  <button type="button" data-bs-target="#carouselExampleFade" data-bs-slide-to="13" aria-label="Slide 14"></button>
+                  <button type="button" data-bs-target="#carouselExampleFade" data-bs-slide-to="14" aria-label="Slide 15"></button>
+                  <button type="button" data-bs-target="#carouselExampleFade" data-bs-slide-to="15" aria-label="Slide 16"></button>
+                  <button type="button" data-bs-target="#carouselExampleFade" data-bs-slide-to="16" aria-label="Slide 17"></button>
+                  <button type="button" data-bs-target="#carouselExampleFade" data-bs-slide-to="17" aria-label="Slide 18"></button>
+                  <button type="button" data-bs-target="#carouselExampleFade" data-bs-slide-to="18" aria-label="Slide 19"></button>
+                  <button type="button" data-bs-target="#carouselExampleFade" data-bs-slide-to="19" aria-label="Slide 20"></button>
+                  <button type="button" data-bs-target="#carouselExampleFade" data-bs-slide-to="20" aria-label="Slide 21"></button>
+                  <button type="button" data-bs-target="#carouselExampleFade" data-bs-slide-to="21" aria-label="Slide 22"></button>
+                  <button type="button" data-bs-target="#carouselExampleFade" data-bs-slide-to="22" aria-label="Slide 23"></button>
+                  <button type="button" data-bs-target="#carouselExampleFade" data-bs-slide-to="23" aria-label="Slide 24"></button>
+                  <button type="button" data-bs-target="#carouselExampleFade" data-bs-slide-to="24" aria-label="Slide 25"></button>
+                  <button type="button" data-bs-target="#carouselExampleFade" data-bs-slide-to="25" aria-label="Slide 26"></button>
+                  <button type="button" data-bs-target="#carouselExampleFade" data-bs-slide-to="26" aria-label="Slide 27"></button>
+                  <button type="button" data-bs-target="#carouselExampleFade" data-bs-slide-to="27" aria-label="Slide 28"></button>
+                  <button type="button" data-bs-target="#carouselExampleFade" data-bs-slide-to="28" aria-label="Slide 29"></button>
+                  <button type="button" data-bs-target="#carouselExampleFade" data-bs-slide-to="29" aria-label="Slide 30"></button>
+                  <button type="button" data-bs-target="#carouselExampleFade" data-bs-slide-to="30" aria-label="Slide 31"></button>
+                  <button type="button" data-bs-target="#carouselExampleFade" data-bs-slide-to="31" aria-label="Slide 32"></button>
+                  <button type="button" data-bs-target="#carouselExampleFade" data-bs-slide-to="32" aria-label="Slide 33"></button>
+                  
               </div>
               <div className="carousel-inner">
                   <div className="carousel-item active">
@@ -59,6 +106,75 @@ function EngagementCarousel() {
                   </div>
                   <div className="carousel-item">
                       <img src={carouselPhoto10} className="d-block" width="100%" height="700px" alt="..."/>
+                  </div>
+                  <div className="carousel-item">
+                      <img src={carouselPhoto11} className="d-block" width="100%" height="700px" alt="..."/>
+                  </div>
+                  <div className="carousel-item">
+                      <img src={carouselPhoto12} className="d-block" width="100%" height="700px" alt="..."/>
+                  </div>
+                  <div className="carousel-item">
+                      <img src={carouselPhoto13} className="d-block" width="100%" height="700px" alt="..."/>
+                  </div>
+                  <div className="carousel-item">
+                      <img src={carouselPhoto14} className="d-block" width="100%" height="700px" alt="..."/>
+                  </div>
+                  <div className="carousel-item">
+                      <img src={carouselPhoto15} className="d-block" width="100%" height="700px" alt="..."/>
+                  </div>
+                  <div className="carousel-item">
+                      <img src={carouselPhoto16} className="d-block" width="100%" height="700px" alt="..."/>
+                  </div>
+                  <div className="carousel-item">
+                      <img src={carouselPhoto17} className="d-block" width="100%" height="700px" alt="..."/>
+                  </div>
+                  <div className="carousel-item">
+                      <img src={carouselPhoto18} className="d-block" width="100%" height="700px" alt="..."/>
+                  </div>
+                  <div className="carousel-item">
+                      <img src={carouselPhoto19} className="d-block" width="100%" height="700px" alt="..."/>
+                  </div>
+                  <div className="carousel-item">
+                      <img src={carouselPhoto20} className="d-block" width="100%" height="700px" alt="..."/>
+                  </div>
+                  <div className="carousel-item">
+                      <img src={carouselPhoto21} className="d-block" width="100%" height="700px" alt="..."/>
+                  </div>
+                  <div className="carousel-item">
+                      <img src={carouselPhoto22} className="d-block" width="100%" height="700px" alt="..."/>
+                  </div>
+                  <div className="carousel-item">
+                      <img src={carouselPhoto23} className="d-block" width="100%" height="700px" alt="..."/>
+                  </div>
+                  <div className="carousel-item">
+                      <img src={carouselPhoto24} className="d-block" width="100%" height="700px" alt="..."/>
+                  </div>
+                  <div className="carousel-item">
+                      <img src={carouselPhoto25} className="d-block" width="100%" height="700px" alt="..."/>
+                  </div>
+                  <div className="carousel-item">
+                      <img src={carouselPhoto26} className="d-block" width="100%" height="700px" alt="..."/>
+                  </div>
+                  <div className="carousel-item">
+                      <img src={carouselPhoto27} className="d-block" width="100%" height="700px" alt="..."/>
+                  </div>
+                  <div className="carousel-item">
+                      <img src={carouselPhoto28} className="d-block" width="100%" height="700px" alt="..."/>
+                  </div>
+                  <div className="carousel-item">
+                      <img src={carouselPhoto29} className="d-block" width="100%" height="700px" alt="..."/>
+                  </div>
+                  <div className="carousel-item">
+                      <img src={carouselPhoto30} className="d-block" width="100%" height="700px" alt="..."/>
+                  </div>
+                  <div className="carousel-item">
+                      <img src={carouselPhoto31} className="d-block" width="100%" height="700px" alt="..."/>
+                  </div>
+                  <div className="carousel-item">
+                      <img src={carouselPhoto32} className="d-block" width="100%" height="700px" alt="..."/>
+                  </div>
+                  <div className="carousel-item">
+                      <img src={carouselPhoto33} className="d-block" width="100%" height="700px" alt="..."/>
                   </div>
               </div>
               <button className="carousel-control-prev" type="button" data-bs-target="#carouselExampleFade" data-bs-slide="prev">
