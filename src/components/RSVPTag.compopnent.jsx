7170f5097c6894cb4f9a782c19d90aa8aa379a5a
MichaelSault <michael_sault@hotmail.com>
2024-01-24 00:24:41 -0500

    front-end layout now resizes responsively based on screen size

--- src/components/RSVPTag.compopnent.jsx
+++ src/components/RSVPTag.compopnent.jsx
@@ -7,10 +7,10 @@ function RSVPTag() {
     return (
         <>
             <div className='rsvpRow'>
-                
-                        <h6>We hope to see you from the altar.</h6>
-                    <hr/>
-
+            <div className='rsvpTitle'>
+                    <h6>We hope to see you from the altar.</h6>
+                    <hr className='hrRSVP'/>
+            </div>
                 <div className='rsvpDeadline'>
                     <h7>We kindly request your response by May 6, 2024.  Cheers!</h7>
                     <button className='rsvpTagButton'>RSVP now</button>
--- src/components/attire.component.jsx
+++ src/components/attire.component.jsx
@@ -15,21 +15,21 @@ function Attire() {
                 <div className='attireQuestionsCol'>
                     <div className='attireTitleSubCol'>
                         <h2>WHAT DO I WEAR TO THE ANAND KARAJ?</h2>
-                        <hr/>
+                        <hr className='hrAttire'/>
                         <h2>You will be required to sit on the ground during the ceremony for approximately 1 hour. Comfortable pants, long dresses and modest tops are ideal. Most men wear suits. Your head will also need to be covered in the gurdwara (Sikh temple). Head coverings will be provided for men. Ladies will need to bring a shawl to drape over their heads. You will also be required to remove your shoes at the temple. </h2>
-                        <hr/>
+                        <hr className='hrAttire'/>
                         <h2>WHAT DO I WEAR TO THE CIVIL/CEREMONY RECEPTION?</h2>
-                        <hr/>
+                        <hr className='hrAttire'/>
                         <h2>Formal attire is requested. You can wear Western or South Asian formalwear. Either is appropriate and appreciated, so wear whatever you feel your best in!  </h2>
-                        <hr/>
+                        <hr className='hrAttire'/>
                         <h2>WHERE CAN I PURCHASE INDIAN CLOTHING?</h2>
-                        <hr/>
+                        <hr className='hrAttire'/>
                         <h2>We suggest reaching out to any South Asian friends you can borrow from. There are several boutiques in Brampton and Toronto where you can buy clothes. If you are interested in renting clothing, Meera Meera Rentals is an excellent choice. You can also find indian formalwear on online retailers such as Etsy, Amazon, House of Indya, and The Saree Room. </h2>
                         
                     </div>
                 </div>
                 <div className='attirePhotoCol'>
-                        <img src={AttireExample} className='scheduleImageSubCol' alt="Wedding Photo"/>
+                        <img src={AttireExample} className='attireImage' alt="Wedding Photo"/>
                 </div>
             </div>
         </>
