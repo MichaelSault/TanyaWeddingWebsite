d8f0fb46abb4b60238969878d1e0f57221067749
MichaelSault <michael_sault@hotmail.com>
2024-04-13 12:14:54 -0400

    config changes

--- server.js
+++ server.js
@@ -19,9 +19,9 @@ app.use(express.urlencoded({extended: false}));
 app.use(cors());
 
 if (process.env.NODE_ENV === 'production') {
-    app.use(express.static('build'));
+    app.use(express.static('dist'));
     app.get('*', (req, res) => {
-    res.sendFile(path.join(__dirname + '/build/index.html'));
+    res.sendFile(path.join(__dirname + '/dist/index.html'));
     });
 }
 
